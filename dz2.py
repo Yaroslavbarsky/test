e298fd6206b3d56a2be524ec9df9c799b766448b
Barsky Yaroslav <yaroslavbarsky@gmail.com>
2021-04-17 17:04:34 +0300

    dz1704

--- dz2.py
+++ dz2.py
@@ -1,19 +1,9 @@
-bred = 10 
-ice_cream = 25 
-macarons = 50 
-snicers = 15 
-sale = 0.1 
-a = input('введите что ви хотите купить : bred ,ice_cream , macarons , snicers ')
-def calculate_price(line):
-    if a == 'bred' :
-        print(' внесите 9 гривен')
-    if a == 'ice_cream' :
-        print(' внесите 22.5 гривен') 
-    if a == 'macarons' :
-        print(' внесите 45 гривен')
-    if a == 'snicers' :
-        print(' внесите 13.5 гривен')
-    else:
-        print('етого товара нет в наличии')
-calculate_price(line)
-    
+number=int(input())
+answer=0
+while True:
+    if number %2 == 0:
+        answer+=1
+        number/=2
+    if number %2 != 0:
+        break
+print(answer)
